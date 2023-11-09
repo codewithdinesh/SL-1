a5f0e1345f8f1e759a9743ea91c3249c773e3789
codewithdinesh <pr760677@gmail.com>
2023-11-09 07:10:53 +0530

    Code Modified

--- AI/AI_2.py
+++ AI/AI_2.py
@@ -1,68 +1,108 @@
-# Dinesh Rathod(TA57) - Experiment 2: 8 Puzzle Problem
+from heapq import heappush, heappop
+import copy
 
-import heapq
+n = 3
 
+rows = [1, 0, -1, 0]
+cols = [0, -1, 0, 1]
 
-class EightPuzzle:
-    def __init__(self, initial_state):
-        self.initial_state = initial_state
-        self.goal_state = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-        self.actions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-    def is_valid(self, state):
-        return set(state) == set(self.goal_state)
-
-    def get_blank_position(self, state):
-        blank_index = state.index(0)
-        return blank_index // 3, blank_index % 3
-
-    def get_neighbors(self, state):
-        x, y = self.get_blank_position(state)
-        neighbors = []
-        for dx, dy in self.actions:
-            new_x, new_y = x + dx, y + dy
-            if 0 <= new_x < 3 and 0 <= new_y < 3:
-                new_state = state[:]
-                new_index = new_x * 3 + new_y
-                new_state[x * 3 + y], new_state[new_index] = new_state[new_index], new_state[x * 3 + y]
-                neighbors.append(new_state)
-        return neighbors
-
-    def heuristic(self, state):
-        return sum(1 for i, j in zip(state, self.goal_state) if i != j)
-
-    def a_star_search(self):
-        open_list = [
-            (self.initial_state, 0, self.heuristic(self.initial_state))]
-        closed_set = set()
-        g_values = {tuple(self.initial_state): 0}
-
-        while open_list:
-            state, g, h = heapq.heappop(open_list)
-            if self.is_valid(state):
-                return state
-            if state in closed_set:
-                continue
-            closed_set.add(state)
-
-            for neighbor in self.get_neighbors(state):
-                if neighbor not in closed_set:
-                    new_g = g + 1
-                    if new_g < g_values.get(tuple(neighbor), float('inf')):
-                        g_values[tuple(neighbor)] = new_g
-                        f = new_g + self.heuristic(neighbor)
-                        heapq.heappush(open_list, (neighbor, new_g, f))
-
-        return None
+# Priority Queue
 
 
-# Example usage:
-initial_state = [2, 8, 3, 1, 6, 4, 7, 0, 5]
-puzzle = EightPuzzle(initial_state)
-solution = puzzle.a_star_search()
-if solution:
-    print("Solution found:")
-    for i in range(0, 9, 3):
-        print(solution[i:i+3])
-else:
-    print("No solution found.")
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, key):
+        heappush(self.heap, key)
+
+    def pop(self):
+        return heappop(self.heap)
+
+    def empty(self):
+        return not self.heap
+
+# Node class
+
+
+class Node:
+    def __init__(self, parent, state, empty_pos, cost, level):
+        self.parent = parent
+        self.state = state
+        self.empty_pos = empty_pos
+        self.cost = cost
+        self.level = level
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+# Calculate the Manhattan distance heuristic
+
+
+def manhattan_distance(state, goal):
+    distance = 0
+    for i in range(n):
+        for j in range(n):
+            if state[i][j] != 0:
+                x, y = divmod(state[i][j] - 1, n)
+                distance += abs(x - i) + abs(y - j)
+    return distance
+
+# Generate new nodes by moving the empty tile
+
+
+def generate_new_nodes(node, goal):
+    new_nodes = []
+    x, y = node.empty_pos
+    for dx, dy in zip(rows, cols):
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < n and 0 <= new_y < n:
+            new_state = copy.deepcopy(node.state)
+            new_state[x][y], new_state[new_x][new_y] = new_state[new_x][new_y], new_state[x][y]
+            new_empty_pos = (new_x, new_y)
+            cost = manhattan_distance(new_state, goal) + node.level + 1
+            new_nodes.append(
+                Node(node, new_state, new_empty_pos, cost, node.level + 1))
+    return new_nodes
+
+# Print the path from the initial state to the goal state
+
+
+def print_path(node):
+    path = []
+    while node:
+        path.insert(0, node.state)
+        node = node.parent
+    for state in path:
+        for row in state:
+            print(" ".join(map(str, row)))
+        print()
+
+# Solve the 8-puzzle problem using A* with Branch and Bound
+
+
+def solve_8_puzzle(initial, goal):
+    start = Node(None, initial, (0, 0), manhattan_distance(initial, goal), 0)
+    open_list = PriorityQueue()
+    open_list.push(start)
+
+    while not open_list.empty():
+        current_node = open_list.pop()
+
+        if current_node.state == goal:
+            print("Path to the goal state:")
+            print_path(current_node)
+            return
+
+        new_nodes = generate_new_nodes(current_node, goal)
+        for new_node in new_nodes:
+            open_list.push(new_node)
+
+
+# Example initial and goal states
+initial_state = [[1, 2, 3], [4, 0, 6], [7, 5, 8]]
+goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+# Solve the 8-puzzle problem
+print("Solving the 8-puzzle problem:")
+solve_8_puzzle(initial_state, goal_state)
